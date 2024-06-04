2d5433bfdaa071c5ae670e6b1c0b9344b12e0e9d
Tamika Davis <107959611+goddessredd@users.noreply.github.com>
2024-06-04 01:16:53 -0400

    added var with if/else alerts to js file

---
+++ script.js
@@ -0,0 +1,12 @@
+var awesomeBirds = ["Bluebird", "Roadrunner", "Desert Dove", "Pigeon"];
+
+    for (let i = 0; i < awesomeBirds.lenght; i++) {
+    if (awesomeBirds[i] === "Bluebird") {
+        alert("Bluebirds are so beautiful");
+
+    }
+    else {
+       alert("Not as beautiful as Bluebirds");
+
+    }
+}
